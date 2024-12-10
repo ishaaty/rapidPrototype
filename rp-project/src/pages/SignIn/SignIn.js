4094c805d4d242f27fda3a0337c780d4efe0c580
J-Tell <fairyq007@hotmail.com>
2024-12-10 10:47:55 -0500

    Deca Logo has been added to the welcome page

--- rp-project/src/pages/SignIn/SignIn.js
+++ rp-project/src/pages/SignIn/SignIn.js
@@ -5,8 +5,10 @@ export default function SignIn() {
     return (
         <>
             <Header />
-            <h2 class="bgcolor">Sign In</h2>
-            <image></image>
+            <h2>Sign In</h2>
+            <div>
+                <img src="decaLogo.png" alt="decaLogoNotWorking"></img>
+            </div>
             <div class="button-container">
                 <button id="signupbtn"><strong>Sign Up</strong></button>
                 <button id="loginbtn"><strong>Log In</strong></button>
