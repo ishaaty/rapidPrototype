dba5204bca878db91b96bc13078ee0f7273715ef
J-Tell <fairyq007@hotmail.com>
2024-12-10 10:38:53 -0500

    Everything is no longer yellow

--- rp-project/src/pages/SignIn/SignIn.js
+++ rp-project/src/pages/SignIn/SignIn.js
@@ -5,7 +5,8 @@ export default function SignIn() {
     return (
         <>
             <Header />
-            <h2>Sign In</h2>
+            <h2 class="bgcolor">Sign In</h2>
+            <image></image>
             <div class="button-container">
                 <button id="signupbtn"><strong>Sign Up</strong></button>
                 <button id="loginbtn"><strong>Log In</strong></button>
