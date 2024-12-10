e8f54e7580c1c3175d8fa061c927aa09abb3f3fd
Isha Tyagi <ishatyagi888@gmail.com>
2024-12-10 10:34:33 -0500

    Made header

--- rp-project/src/components/Header/Header.js
+++ rp-project/src/components/Header/Header.js
@@ -1,7 +1,20 @@
 // containers deca logo, about deca page, "abf-ib," and contact/tech support page
 
-export default function Header(){
+import React from 'react';
+import './Header.css'; // Import the CSS file
+
+export default function Header() {
     return (
-        <h1>Sample Header</h1>
-    )
+        <header className="header">
+            <div className="header-logo-container">
+                <img src="/decaImg.png" alt="Logo" className="header-logo" />
+            </div>
+            <nav className="header-nav">
+                <a href="home" className="header-link" style={{ color: "#F5585E" }}>Home</a>
+                <a href="about" className="header-link" style={{ color: "#FFC551" }}>About</a>
+                <a href="contact" className="header-link" style={{ color: "#00984D" }}>Contact</a>
+            </nav>
+        </header>
+    );
 }
+
