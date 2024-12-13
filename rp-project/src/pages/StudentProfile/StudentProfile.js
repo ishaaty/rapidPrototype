43b676b3937749bb5d2250fd0d3b343aade3434c
Isha Tyagi <ishatyagi888@gmail.com>
2024-12-13 18:55:16 -0500

    added user roles to competition pages

--- rp-project/src/pages/StudentProfile/StudentProfile.js
+++ rp-project/src/pages/StudentProfile/StudentProfile.js
@@ -2,35 +2,70 @@ import './StudentProfile.css';
 import Header from '../../components/Header/Header'
 import Menu from '../../components/Menu/Menu'
 
-export default function StudentProfile() {
-    return (
-        <>
+export default function StudentProfile(props) {
 
-            <Header />
-            <Menu />
-
-            <div style={{ display: "flex", flexDirection: "column", alignItems: "center", justifyContent: "center", height: "100vh", gap: "20px" }}>
-
-                <h1 className="header-text">Student Profile (Roleplays)</h1>
-
-                <button style={{ backgroundColor: "#E3E8F1", borderRadius: "20px", padding: "30px", width: "400px" }}>
-                    <h1>To Do List</h1>
-                    <h2 style={{ color: "#FF0303" }}> X NJ DECA Form</h2>
-                    <h2 style={{ color: "#00984D" }}>✓ Script</h2>
-                </button>
-
-                <button style={{ backgroundColor: "#E3E8F1", borderRadius: "20px", padding: "30px", width: "400px" }}>
-                    <h1>Add Comment</h1>
-                    <div style={{ display: "flex", flexDirection: "column", alignItems: "center", justifyContent: "center", gap: "20px" }}>
-                        <button style={{ backgroundColor: "white", borderRadius: "20px", padding: "30px", width: "200px"}}></button>
-                        <button style={{ backgroundColor: "#00529B", color: "white", padding: "10px", borderRadius: "20px" }}>Submit</button>
-                    </div>
+    if (props.userRole === "admin"){
+        return (
+            <>
+    
+                <Header />
+                <Menu />
+    
+                <div style={{ display: "flex", flexDirection: "column", alignItems: "center", justifyContent: "center", height: "100vh", gap: "20px" }}>
+    
+                    <h1 className="header-text">Student Profile (Roleplays)</h1>
+    
+                    <button style={{ backgroundColor: "#E3E8F1", borderRadius: "20px", padding: "30px", width: "400px" }}>
+                        <h1>To Do List</h1>
+                        <h2 style={{ color: "#FF0303" }}> X NJ DECA Form</h2>
+                        <h2 style={{ color: "#00984D" }}>✓ Script</h2>
+                    </button>
+    
+                    <button style={{ backgroundColor: "#E3E8F1", borderRadius: "20px", padding: "30px", width: "400px" }}>
+                        <h1>Add Comment</h1>
+                        <div style={{ display: "flex", flexDirection: "column", alignItems: "center", justifyContent: "center", gap: "20px" }}>
+                            <button style={{ backgroundColor: "white", borderRadius: "20px", padding: "30px", width: "200px"}}></button>
+                            <button style={{ backgroundColor: "#00529B", color: "white", padding: "10px", borderRadius: "20px" }}>Submit</button>
+                        </div>
+                        
+                    </button>
+    
+                </div>
+    
                     
-                </button>
-
-            </div>
-
-                
-        </>
-    )
+            </>
+        )
+    } else {
+        return (
+            <>
+    
+                <Header />
+                <Menu />
+    
+                <div style={{ display: "flex", flexDirection: "column", alignItems: "center", justifyContent: "center", height: "100vh", gap: "20px" }}>
+    
+                    <h1 className="header-text">Student Profile (Roleplays)</h1>
+    
+                    <button style={{ backgroundColor: "#E3E8F1", borderRadius: "20px", padding: "30px", width: "400px" }}>
+                        <h1>To Do List</h1>
+                        <h2 style={{ color: "#FF0303" }}> X NJ DECA Form</h2>
+                        <h2 style={{ color: "#00984D" }}>✓ Script</h2>
+                    </button>
+    
+                    <button style={{ backgroundColor: "#E3E8F1", borderRadius: "20px", padding: "30px", width: "400px" }}>
+                        <h1>Upload Materials</h1>
+                        <div style={{ display: "flex", flexDirection: "column", alignItems: "center", justifyContent: "center", gap: "20px" }}>
+                            <strong><h2>Type of Submission</h2></strong>
+                            <h2>Upload Here: Choose from Files</h2>
+                            <button style={{ backgroundColor: "#00529B", color: "white", padding: "10px", borderRadius: "20px" }}>Submit</button>
+                        </div>
+                        
+                    </button>
+    
+                </div>
+    
+                    
+            </>
+        )
+    }
 }
--- rp-project/src/pages/ViewAddEvents/ViewAddEvents.js
+++ rp-project/src/pages/ViewAddEvents/ViewAddEvents.js
@@ -2,60 +2,122 @@ import './ViewAddEvents.css';
 import Header from '../../components/Header/Header'
 import Menu from '../../components/Menu/Menu'
 
-export default function ViewAddEvents() {
-    return (
-        <>
-            <Header />
-            <Menu />
-            <div style={{ color: "#00529B", alignItems: "center" }} className="add-event-container">
-                <h1 style={{ color: "#00529B" }}>View & Add Events: Regionals</h1>
-                <a href="roommates">
-                    <button style={{ backgroundColor: "#00529B", color:"white" }}>
-                        View Roommates
-                    </button>
-                </a>
-                <div>
-                <h1 style={{ color: "#F5585E", alignItems: "center" }}>Current Events:</h1>
-                    <button id="roleplay" style={{ backgroundColor: "#E3E8F1", color: "#6786A3", marginRight: "45px"}}>
-                        <h1>Roleplays</h1>
-                        <h2>Description: </h2>
-                        <h2>Date: Time</h2>
-                        <a href="viewrequesters">
-                            <button style={{ backgroundColor: "#6786A3", color: "white"}}>View Requesters</button>
-                        </a>
-                    </button>
+export default function ViewAddEvents(props) {
 
-                    <button id="binder" style={{ backgroundColor: "#E3E8F1", color: "#6786A3", marginRight: "45px"}}>
-                        <h1>Binders</h1>
-                        <h2>Description: </h2>
-                        <h2>Date: Time</h2>
-                        <button style={{ backgroundColor: "#6786A3", color: "white"}}>View Requesters</button>
+    if (props.userRole === "admin"){
+        return (
+            <>
+                <Header />
+                <Menu />
+                <div style={{ color: "#00529B", alignItems: "center" }} className="add-event-container">
+                    <h1 style={{ color: "#00529B" }}>View & Add Events: Regionals</h1>
+                    <a href="roommates">
+                        <button style={{ backgroundColor: "#00529B", color:"white" }}>
+                            View Roommates
+                        </button>
+                    </a>
+                    <div>
+                        <h1 style={{ color: "#F5585E", alignItems: "center" }}>Current Events:</h1>
+                        <button id="roleplay" style={{ backgroundColor: "#E3E8F1", color: "#6786A3", marginRight: "45px"}}>
+                            <h1>Roleplays</h1>
+                            <h2>Description: </h2>
+                            <h2>Date: Time</h2>
+                            <a href="viewrequesters">
+                                <button style={{ backgroundColor: "#6786A3", color: "white"}}>View Requesters</button>
+                            </a>
+                        </button>
+
+                        <button id="binder" style={{ backgroundColor: "#E3E8F1", color: "#6786A3", marginRight: "45px"}}>
+                            <h1>Binders</h1>
+                            <h2>Description: </h2>
+                            <h2>Date: Time</h2>
+                            <button style={{ backgroundColor: "#6786A3", color: "white"}}>View Requesters</button>
+                        </button>
+                    </div>
+                </div>
+
+                <div class="add-event-container">
+                    <h1 style={{ color: "#F5585E", alignItems: "center" }}>Add Event:</h1>
+                    <button>
+                        <h3>Name</h3>
+                    </button>
+                    <button>
+                        <h3>Description</h3>
+                    </button>
+                    <button>
+                        <h3>Date/Time</h3>
+                    </button>
+                    <button>
+                        <h3>Requirement1</h3>
+                    </button>
+                    <button>
+                        <h3>Requirment2</h3>
+                    </button>
+                    <button id="submit-btn">
+                        <h2>Submit</h2>
                     </button>
                 </div>
-            </div>
 
-            <div class="add-event-container">
-                <h1 style={{ color: "#F5585E", alignItems: "center" }}>Add Event:</h1>
-                <button>
-                    <h3>Name</h3>
-                </button>
-                <button>
-                    <h3>Description</h3>
-                </button>
-                <button>
-                    <h3>Date/Time</h3>
-                </button>
-                <button>
-                    <h3>Requirement1</h3>
-                </button>
-                <button>
-                    <h3>Requirment2</h3>
-                </button>
-                <button id="submit-btn">
-                    <h2>Submit</h2>
-                </button>
-            </div>
+            </>
+        )
 
-        </>
-    )
+    } else {
+
+        return (
+            <>
+                <Header />
+                <Menu />
+                <div style={{ color: "#00529B", alignItems: "center" }} className="add-event-container">
+                    <h1 style={{ color: "#00529B" }}>View & Add Events: Regionals</h1>
+                    <a href="roommates">
+                        <button style={{ backgroundColor: "#00529B", color:"white" }}>
+                            View Roommates
+                        </button>
+                    </a>
+                    <div>
+                        <h1 style={{ color: "#F5585E", alignItems: "center" }}>Available Events:</h1>
+                        <button id="roleplay" style={{ backgroundColor: "#E3E8F1", color: "#6786A3", marginRight: "45px"}}>
+                            <h1>Roleplays</h1>
+                            <h2>Description: </h2>
+                            <h2>Date: Time</h2>
+                        </button>
+
+                        <button id="binder" style={{ backgroundColor: "#E3E8F1", color: "#6786A3", marginRight: "45px"}}>
+                            <h1>Binders</h1>
+                            <h2>Description: </h2>
+                            <h2>Date: Time</h2>
+                        </button>
+                    </div>
+                    <div>
+                        <h1 style={{ color: "#F5585E", alignItems: "center" }}>My Events:</h1>
+                        <a href="studentprofile">
+                            <button id="roleplay" style={{ backgroundColor: "#E3E8F1", color: "#6786A3", marginRight: "45px"}}>
+                                <h1>Roleplays</h1>
+                                <h2>Description: </h2>
+                                <h2>Date: Time</h2>
+                            </button>
+                        </a>
+                    </div>
+                </div>
+
+                <div class="add-event-container">
+                    <h1 style={{ color: "#F5585E", alignItems: "center" }}>Ranks Events:</h1>
+                    <button>
+                        <h3>Choice 1</h3>
+                    </button>
+                    <button>
+                        <h3>Choice 2</h3>
+                    </button>
+                    <button>
+                        <h3>Choice 3</h3>
+                    </button>
+                    <button id="submit-btn">
+                        <h2>Submit</h2>
+                    </button>
+                </div>
+
+            </>
+        )
+
+    }
 }
