fd353fb756d3ee0ab26dedb7e4b619a6f3472e00
ashjoh25 <ajohnson100920@gmail.com>
2024-12-12 20:25:22 -0500

    resources page with admin participant views

--- rp-project/src/pages/Resources/Resources.js
+++ rp-project/src/pages/Resources/Resources.js
@@ -1,47 +1,41 @@
 import './Resources.css';
-import Header from '../../components/Header/Header'
-import Menu from '../../components/Menu/Menu'
-import Resources from '../../components/Resources/Resources'
+import Header from '../../components/Header/Header';
+import Menu from '../../components/Menu/Menu';
+import Resources from '../../components/Resources/Resources';
 
+const ResourcesPage = () => {
+  const userRole = 'admin'; // FOR TESTING MAKE IT 'admin' TO TEST ADMIN OR ANYTHING ELSE TO TEST PARTICIPANT
+  const resourcesData = [
+    {
+      text: 'Deca+',
+      link1: 'https://example.com/link1',
+      link2: 'https://example.com/link2',
+    },
+    {
+      text: 'Naviance',
+      link1: 'https://example.com/link3',
+      link2: 'https://example.com/link4',
+    },
+    {
+      text: 'PDF Forms',
+      link1: 'https://example.com/link5',
+      link2: 'https://example.com/link6',
+    },
+    {
+      text: 'Community Pass',
+      link1: 'https://example.com/link7',
+      link2: 'https://example.com/link8',
+    },
+  ];
 
-const ResourcesPage = (userRole) => {
-    const resourcesData = [
-      {
-        text: "Deca+",
-        link1: "https://example.com/link1",
-        link1Label: "Delete",
-        link2: "https://example.com/link2",
-        link2Label: "Edit",
-      },
-      {
-        text: "Naviance",
-        link1: "https://example.com/link3",
-        link1Label: "Delete",
-        link2: "https://example.com/link4",
-        link2Label: "Edit",
-      },
-      {
-        text: "PDF Forms",
-        link1: "https://example.com/link3",
-        link1Label: "Delete",
-        link2: "https://example.com/link4",
-        link2Label: "Edit",
-      },
-      {
-        text: "Community Pass",
-        link1: "https://example.com/link3",
-        link1Label: "Delete",
-        link2: "https://example.com/link4",
-        link2Label: "Edit",
-      },
-    ];
-  
-    return (
-      <div className="resources-page">
-        <h1>Resources</h1>
-        <Resources resources={resourcesData} />
-      </div>
-    );
-  };
-  
-  export default ResourcesPage;
+  return (
+    <div className="resources-page">
+      <Header />
+      <Menu />
+      <h1>Resources</h1>
+      <Resources resources={resourcesData} userRole={userRole} />
+    </div>
+  );
+};
+
+export default ResourcesPage;
