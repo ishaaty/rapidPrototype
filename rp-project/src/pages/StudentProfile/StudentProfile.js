faab2ea81b129895112f5ef92e59d04854e182b1
Isha Tyagi <ishatyagi888@gmail.com>
2024-12-13 10:39:19 -0500

    Officially finished admin views for competitions page

--- rp-project/src/pages/StudentProfile/StudentProfile.js
+++ rp-project/src/pages/StudentProfile/StudentProfile.js
@@ -7,20 +7,28 @@ export default function StudentProfile() {
         <>
 
             <Header />
+            <Menu />
 
-            <h1 className="header-text">Student Profile (Roleplays)</h1>
+            <div style={{ display: "flex", flexDirection: "column", alignItems: "center", justifyContent: "center", height: "100vh", gap: "20px" }}>
 
-            <button>
-                <h1>To Do List</h1>
-                <h2>NJ DECA Form</h2>
-                <h2>Script</h2>
-            </button>
+                <h1 className="header-text">Student Profile (Roleplays)</h1>
 
-            <button>
-                <h1>Add Comment</h1>
-                <button></button>
-                <button>Submit</button>
-            </button>
+                <button style={{ backgroundColor: "#E3E8F1", borderRadius: "20px", padding: "30px", width: "400px" }}>
+                    <h1>To Do List</h1>
+                    <h2 style={{ color: "#FF0303" }}> X NJ DECA Form</h2>
+                    <h2 style={{ color: "#00984D" }}>✓ Script</h2>
+                </button>
+
+                <button style={{ backgroundColor: "#E3E8F1", borderRadius: "20px", padding: "30px", width: "400px" }}>
+                    <h1>Add Comment</h1>
+                    <div style={{ display: "flex", flexDirection: "column", alignItems: "center", justifyContent: "center", gap: "20px" }}>
+                        <button style={{ backgroundColor: "white", borderRadius: "20px", padding: "30px", width: "200px"}}></button>
+                        <button style={{ backgroundColor: "#00529B", color: "white", padding: "10px", borderRadius: "20px" }}>Submit</button>
+                    </div>
+                    
+                </button>
+
+            </div>
 
                 
         </>
